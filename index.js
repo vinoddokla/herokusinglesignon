47ddd5bc340b5e4524c540ef66f2bb3364e1e232
vinod.patel <vinod.patel@financialforce.com>
2019-02-05 16:46:36 +0530

    login callback

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ passport.use(new facebookStrategy({
 	},
 	function(accessToken, refreshToken, profile, cb) {
 		console.log(profile);
-		return cb(err, profile);
+		return cb(null, profile);
 	}
 ));
 
