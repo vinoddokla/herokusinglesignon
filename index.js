4eae90f1d91dde3d7c79dcde7b59a48db6ec9b2f
vinod.patel <vinod.patel@financialforce.com>
2019-02-05 15:54:31 +0530

    login with facebook with basic data fetch

--- index.js
+++ index.js
@@ -3,27 +3,56 @@ const app = express();
 const port = 3000;
 const path = require('path');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const facebookStrategy = require('passport-facebook');
 
+const FACEBOOK_APP_ID = '333877367228716',
+	FACEBOOK_APP_SECRET = 'c603984f918b6402f92e075ea9065149';
+
+// const facebookStrategyOPT = {
+// 	cliendID : '333877367228716',
+// 	clientSecret : 'c603984f918b6402f92e075ea9065149',
+// 	callbackURL : 'https://localhost:3000/auth/facebook/callback'
+// };
+
+// const fbCallback = function(accessToken, refreshToken, profile, cb) {
+// 	console.log(accessToken, refreshToken, profile);
+// };
+
+passport.use(new facebookStrategy({
+		clientID: FACEBOOK_APP_ID,
+		clientSecret: FACEBOOK_APP_SECRET,
+		callbackURL: "http://localhost:3000/auth/facebook/callback"
+	},
+	function(accessToken, refreshToken, profile, cb) {
+		console.log(profile);
+	}
+));
+
+app.post('/', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback',
+	passport.authenticate('facebook', { successRedirect: '/home',
+										failureRedirect: '/index' }));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'pug');
 
 app.get('/', function(req, res) {
-    res.render(path.join(__dirname, 'www/index'));
+	res.render(path.join(__dirname, 'www/index'));
 });
 
-app.get('/logout', function(req, res) {
-    res.render(path.join(__dirname, 'www/index'));
-});
+// app.get('/logout', function(req, res) {
+// 	res.render(path.join(__dirname, 'www/index'));
+// });
 
 app.post('/home', function(req, res) {
-    res.render(path.join(__dirname, 'www/home'));
+	res.render(path.join(__dirname, 'www/home'));
 });
 
-app.post('/', function(req, res) {
-    console.log(req.body);
-    res.send(req.body);
-});
+// app.post('/', function(req, res) {
+// 	console.log(req.body);
+// });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
