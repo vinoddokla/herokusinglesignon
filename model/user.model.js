11d5ead405a90763d2353880f548044857ed492a
vinod.patel <vinod.patel@financialforce.com>
2019-02-06 12:51:06 +0530

    test

--- model/user.model.js
+++ model/user.model.js
@@ -3,20 +3,8 @@ const Schema = mongoose.Schema;
 
 const schema = new Schema({
 		id: { type: String, unique: false, required: false},
-		username: { type: String, unique: false, required: false},
 		displayName: { type: String, unique: false, required: false},
-		name:
-		{ familyName: { type: String, unique: false, required: false},
-			givenName: { type: String, unique: false, required: false},
-			middleName: { type: String, unique: false, required: false} 
-		},
-		gender: { type: String, unique: false, required: false},
-		profileUrl: { type: String, unique: false, required: false},
 		provider: { type: String, unique: false, required: false},
-		_raw: {
-			name:{type: String, unique: false, required: false},
-			id:{type: String, unique: false, required: false}
-		},
 		_json: { 
 			name: { type: String, unique: false, required: false}, 
 			id: { type: String, unique: false, required: false} 
